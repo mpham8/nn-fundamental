673149e5df76bca46ba730146c9a3a9884f81ff4
Michael Pham <pham_michael@outlook.com>
2026-04-26 22:06:01 -0400

    added multihead attn

--- transformer/src/model.py
+++ transformer/src/model.py
@@ -1,6 +1,41 @@
 import torch.nn as nn
 import torch
 
+
+class maskedSingleHeadAttentionBlock(nn.Module):
+    def __init__(self, d_model, d_k, d_v, maxTokens):
+        super().__init__()
+        self.d_k = d_k
+        self.d_v = d_v
+
+        self.W_q = nn.Linear(d_model, d_k)
+        self.W_k = nn.Linear(d_model, d_k)
+        self.W_v = nn.Linear(d_model, d_v)
+
+        self.register_buffer('mask', torch.log(torch.tril(torch.ones(maxTokens, maxTokens)))) #buffer for attention mask so not recreated every forward pass
+
+
+    def forward(self, x):
+        tokens = x.shape[1] #x is batch x tokens x d_model
+
+        Q = self.W_q(x)
+        K = self.W_k(x)
+        V = self.W_v(x)
+
+        mask = self.mask[:tokens, :tokens]
+        return torch.softmax ( (Q @ K.transpose(-2,-1)) / (self.d_k**0.5) + mask, dim=1) @ V  
+
+
+class maskedMultiHeadAttentionBlock(nn.Module):
+    def __init__(self, d_model, d_k, d_v, h):
+        super().__init__()
+        self.heads = nn.ModuleList([maskedSingleHeadAttentionBlock(d_model, d_k, d_v) for i in range(h)]) #modulelist so can repeat
+        self.W_o = nn.Linear(h * d_v, d_model)
+    
+    def forward(self, x):
+        x = torch.cat([head(x) for head in self.heads], dim = -1)
+        return self.W_o(x)
+
 #decoder block
 class decoderBlock(nn.Module):
     def __init__(self):
@@ -18,27 +53,3 @@ class decoderBlock(nn.Module):
         #feed forward
 
         #add and norm
-
-
-        
-
-class attentionBlock(nn.Module):
-    def __init__(self, d_model, d_k, d_v):
-        super().__init__()
-        self.d_k = d_k
-        self.d_v = d_v
-
-        self.W_q = nn.Linear(d_model, d_k)
-        self.W_k = nn.Linear(d_model, d_k)
-        self.W_v = nn.Linear(d_model, d_v)
-
-
-    def forward(self, x):
-        tokens = x.shape[1] #x is batch x tokens x d_model
-
-        Q = self.W_q(x)
-        K = self.W_k(x)
-        V = self.W_v(x)
-
-        mask = torch.log (torch.tril(torch.ones(tokens, tokens)))
-        torch.softmax ( (Q @ K.transpose(-2,-1)) / (self.d_k**0.5) + mask, dim=1) @ V  
