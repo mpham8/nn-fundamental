0fe253c362973b44a029709c65267366a436038e
Michael Pham <pham_michael@outlook.com>
2026-04-27 08:27:22 -0400

    finished model classes

--- transformer/src/model.py
+++ transformer/src/model.py
@@ -2,7 +2,7 @@ import torch.nn as nn
 import torch
 
 
-class maskedSingleHeadAttentionBlock(nn.Module):
+class MaskedSingleHeadAttentionBlock(nn.Module):
     def __init__(self, d_model, d_k, d_v, maxTokens):
         super().__init__()
         self.d_k = d_k
@@ -13,7 +13,6 @@ class maskedSingleHeadAttentionBlock(nn.Module):
         self.W_v = nn.Linear(d_model, d_v)
 
         self.register_buffer('mask', torch.log(torch.tril(torch.ones(maxTokens, maxTokens)))) #buffer for attention mask so not recreated every forward pass
-
 
     def forward(self, x):
         tokens = x.shape[1] #x is batch x tokens x d_model
@@ -26,30 +25,56 @@ class maskedSingleHeadAttentionBlock(nn.Module):
         return torch.softmax ( (Q @ K.transpose(-2,-1)) / (self.d_k**0.5) + mask, dim=1) @ V  
 
 
-class maskedMultiHeadAttentionBlock(nn.Module):
+class MaskedMultiHeadAttentionBlock(nn.Module):
     def __init__(self, d_model, d_k, d_v, h):
         super().__init__()
-        self.heads = nn.ModuleList([maskedSingleHeadAttentionBlock(d_model, d_k, d_v) for i in range(h)]) #modulelist so can repeat
+        self.heads = nn.ModuleList([MaskedSingleHeadAttentionBlock(d_model, d_k, d_v) for i in range(h)]) #modulelist so can repeat
         self.W_o = nn.Linear(h * d_v, d_model)
     
     def forward(self, x):
         x = torch.cat([head(x) for head in self.heads], dim = -1)
         return self.W_o(x)
 
-#decoder block
-class decoderBlock(nn.Module):
-    def __init__(self):
+
+class FeedForwardBlock(nn.Module):
+    def __init__(self, d_model):
         super().__init__()
-        
+        self.fc1 = nn.Linear(d_model, d_model*4)
+        self.sig1 = nn.ReLU()
+        self.fc2 = nn.Linear(d_model*4, d_model)
+
     def forward(self, x):
-        #masked multihead attention
+        x = self.fc1(x)
+        x = self.sig1(x)
+        return self.fc2(x)
 
-        #add and norm
 
-        #multihead attention
+#decoder block
+class DecoderBlock(nn.Module):
+    def __init__(self, d_model, d_k, d_v, h, maxTokens):
+        super().__init__()
+        self.attn = MaskedMultiHeadAttentionBlock(d_model, d_k, d_v, h)
+        self.norm1 = nn.LayerNorm(d_model)
+        self.ff = FeedForwardBlock(d_model)
+        self.norm2 = nn.LayerNorm(d_model)
 
-        #add and norm
+    def forward(self, x):
+        x = self.norm1(x + self.attn(x)) #masked multihead attention + add/norm
+        return self.norm2(x + self.ff(x)) #feed forward + add/norm
 
-        #feed forward
 
-        #add and norm
+#entire transformer
+class Transformer(nn.Module):
+    def __init__(self, d_model, d_k, d_v, h, maxTokens, nx, vocabSize):
+        super().__init__()        
+        self.tokenEmbedding = nn.Embedding(vocabSize, d_model)
+        self.positionalEmbedding = nn.Embedding(maxTokens, d_model)
+        self.decoderBlocks = nn.ModuleList([DecoderBlock(d_model, d_k, d_v, h, maxTokens) for i in range(nx)])
+        self.lin = nn.Linear(d_model, vocabSize)
+
+    def forward(self, x):
+        positions = torch.arange(x.shape[1], device = x.device) #x.shape[1] gets seq len, torch.arange() generaties tensor of position idx, 
+
+        x = self.tokenEmbedding(x) + self.positionalEmbedding(positions)
+        for db in self.decoderBlocks: x = db(x)
+        return self.lin(x)
